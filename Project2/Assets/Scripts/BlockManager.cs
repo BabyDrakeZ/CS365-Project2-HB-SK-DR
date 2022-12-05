31a36ee5062fac86af9ae9189e6cbf8a097ea05d
David Richman <david.c.richman@gmail.com>
2022-12-04 21:13:29 -0500

    array block spawning

--- Project2/Assets/Scripts/BlockManager.cs
+++ Project2/Assets/Scripts/BlockManager.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class BlockManager : MonoBehaviour
 {
-    public struct Brick
-    {
-        public float width;
-        public GameObject prefab;
-        public Brick(float width, GameObject prefab)
-        {
-            this.width = width;
-            this.prefab = prefab;
-        }
-    }
-    public Brick[] bricks;
-    public GameObject[] bricks2;
+    //DEFINES A COLUMN OF BRICKS
+    public float width = 3.2f;
+    public float height = 0.8f;
+    public float offset = 0;
+    public int numColumns = 6;
+    public GameObject[] bricks;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //render bricks from top left corner (where this manager is)
+        for (int i = 0; i < numColumns; i++)
+        {
+            for (int j = 0; j < bricks.Length; j++)
+            {
+                try
+                {
+                    GameObject brick = Instantiate(bricks[j]);
+                    brick.transform.SetParent(this.transform);
+                    Debug.Log(brick.ToString() + "[" + i.ToString() + "," + j.ToString() + "]");
+                    brick.transform.position = new Vector3(this.transform.position.x + i * width, this.transform.position.y - j * height, 0);
+                } catch { Debug.Log("Failed to Instantiate[" + i.ToString() + "," + j.ToString() + "]"); }
+            }
+        }
     }
+
 
     // Update is called once per frame
     void Update()
