4f7ebe2cf88048edfd8116dfc2665a13071b84b9
David Richman <david.c.richman@gmail.com>
2022-12-09 00:55:55 -0500

    adding constants

---
+++ Project2/Assets/Scripts/Constants.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static public Constants C;
+    public Time timeCount;
+    public Time highTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        C = this;
+        
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
