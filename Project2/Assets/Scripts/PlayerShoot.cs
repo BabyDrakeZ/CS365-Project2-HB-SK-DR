5dc2dd9c91ae334900ccefb594b8fe979852363b
firehydrangea <kudricksarah@gmail.com>
2022-12-09 02:36:28 -0500

    stopwatch

--- Project2/Assets/Scripts/PlayerShoot.cs
+++ Project2/Assets/Scripts/PlayerShoot.cs
@@ -11,26 +11,27 @@ public class PlayerShoot : MonoBehaviour
     public GameObject bulletPrefab;
     private GameObject shot;
     private PaddleMove paddle;
-    public TMP_Text lives;
-    public int livescount;
+    public TMP_Text stopwatch;
+    public float time=0f;
     
     // Start is called before the first frame update
     void Start()
     {
         paddle = GameObject.FindGameObjectWithTag("Player").GetComponent<PaddleMove>();
-        lives.text = "lives: " + livescount;
+        stopwatch.text = time.ToString("0.00");
     }
 
     // Update is called once per frame
     void Update()
     {
+        time += Time.deltaTime;
+        stopwatch.text = time.ToString("0.00");
         bool bulletDestroyCase = Input.GetKey(KeyCode.Return); //Set to occur when bullet is off screne.
         if (bulletDestroyCase)
             BulletReset();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Shoot();
-            lives.text = "lives: " + livescount;
         }
     }
     void Shoot()
